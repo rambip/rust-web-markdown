7f4e30351ef77d5b60e04c2ee166dd474542d533
Craig Macomber <CraigM@CraigM.info>
2025-05-06 19:17:50 -0700

    Add tests

--- dioxus-markdown/examples/custom-components/src/main.rs
+++ dioxus-markdown/examples/custom-components/src/main.rs
@@ -1,5 +1,4 @@
 use dioxus::prelude::*;
-
 use dioxus_markdown::*;
 
 static MARKDOWN_SOURCE: &str = r#"
@@ -23,16 +22,10 @@ fn Counter(initial: i32) -> Element {
     let mut count = use_signal(|| initial);
 
     rsx! {
-        div{
-            button {
-                onclick: move |_| count-=1,
-                "-"
-            },
-            "{count}",
-            button {
-                onclick: move |_| count+=1,
-                "+"
-            }
+        div {
+            button { onclick: move |_| count -= 1, "-" }
+            "{count}"
+            button { onclick: move |_| count += 1, "+" }
         }
     }
 }
@@ -40,46 +33,167 @@ fn Counter(initial: i32) -> Element {
 #[component]
 fn ColorBox(children: Element) -> Element {
     rsx! {
-        div{
-            style: "border: 2px solid blue",
-            {children}
-        }
+        div { style: "border: 2px solid blue", {children} }
     }
 }
 
-// create a component that renders a div with the text "Hello, world!"
 #[component]
 fn App() -> Element {
     let mut components = CustomComponents::new();
 
     components.register("Counter", |props| {
         Ok(rsx! {
-            Counter {initial: props.get_parsed_optional("initial")?.unwrap_or(0)}
+            Counter { initial: props.get_parsed_optional("initial")?.unwrap_or(0) }
         })
     });
 
     components.register("box", |props| {
         let children = props.children;
         Ok(rsx! {
-            ColorBox {children}
+            ColorBox { children }
         })
     });
 
     rsx! {
-        h1 {"Source"}
-        Markdown {
-            src: "```md\n{MARKDOWN_SOURCE}\n``"
-        }
+        h1 { "Source" }
+        Markdown { src: "```md\n{MARKDOWN_SOURCE}\n``" }
 
-        h1 {"Result"}
-        Markdown {
-            src: MARKDOWN_SOURCE,
-            components: components
-        }
+        h1 { "Result" }
+        Markdown { src: MARKDOWN_SOURCE, components }
     }
 }
 
 fn main() {
     // launch the web app
     dioxus::launch(App);
+}
+
+/// If default target is set to wasm in .cargo/config.toml, these need a specific target provided to run them
+/// for example `cargo test --target "x86_64-unknown-linux-gnu"`
+#[cfg(test)]
+mod tests {
+    use dioxus::prelude::*;
+    use dioxus_markdown::{CustomComponents, Markdown};
+
+    // From https://dioxuslabs.com/learn/0.6/cookbook/testing/
+    fn assert_rsx_eq(first: Element, second: Element) {
+        let first = dioxus_ssr::render_element(first);
+        let second = dioxus_ssr::render_element(second);
+        pretty_assertions::assert_str_eq!(first, second);
+    }
+
+    // Adapted from https://dioxuslabs.com/learn/0.6/cookbook/testing/
+    fn test_hook_simple(mut check: impl FnMut() + 'static) {
+        fn mock_app() -> Element {
+            rsx! { div {} }
+        }
+
+        let vdom = VirtualDom::new(mock_app);
+
+        vdom.in_runtime(|| {
+            ScopeId::ROOT.in_runtime(|| {
+                check();
+            })
+        })
+    }
+
+    #[test]
+    fn minimal() {
+        test_hook_simple(|| {
+            assert_rsx_eq(
+                rsx! {
+                    Markdown { src: "ZZZ" }
+                },
+                rsx! {
+                    p { style: "", class: "",
+                        span { style: "", class: "", "ZZZ" }
+                    }
+                },
+            )
+        });
+    }
+
+    /// Must be run in a Dioxus runtime
+    fn components() -> CustomComponents {
+        let mut components = CustomComponents::new();
+        components.register("X", |props| Ok(rsx! { "Content" }));
+        components
+    }
+
+    #[test]
+    fn custom() {
+        test_hook_simple(|| {
+            assert_rsx_eq(
+                rsx! {
+                    Markdown { src: "<X>", components: components() }
+                },
+                // TODO: it seems a bit odd that Markdown wraps text in a `p` tag and a span, but doesn't do so when its just a custom component.
+                // TO be more consistent with the below case, maybe everything should always be wrapped in a `p`?
+                rsx! { "Content" },
+            )
+        });
+    }
+
+    #[test]
+    fn custom_plus_text() {
+        test_hook_simple(|| {
+            assert_rsx_eq(
+                rsx! {
+                    Markdown { src: "z<X>", components: components() }
+                },
+                // TODO:
+                // This errors due to producing `<p style="" class=""><span style="" class="">z</span><span style="" class=""><X></span></p>` which is not what is desired.
+                // It seems like the inner `<X>` might not even be escaped properly.
+                // Maybe the desired output is this:
+                rsx! {
+                    p { style: "", class: "",
+                        span { style: "", class: "", "z" }
+                        // If this is the desired output, its really odd the above test with just a custom component isn't wrapped in `p` and a `span`.
+                        span { style: "", class: "", "Content" }
+                    }
+                },
+            )
+        });
+    }
+
+    #[test]
+    fn custom_plus_custom() {
+        test_hook_simple(|| {
+            assert_rsx_eq(
+                rsx! {
+                    Markdown { src: "<X><X>", components: components() }
+                },
+                // TODO:
+                // This errors due to producing `<p style="" class=""><span style="" class="">z</span><span style="" class=""><X></span></p>` which is not what is desired.
+                // It seems like the inner `<X>` might not even be escaped properly.
+                // Maybe the desired output is this:
+                rsx! {
+                    p { style: "", class: "",
+                        span { style: "", class: "", "Content" }
+                        span { style: "", class: "", "Content" }
+                    }
+                },
+            )
+        });
+    }
+
+    #[test]
+    fn custom_line_custom() {
+        test_hook_simple(|| {
+            assert_rsx_eq(
+                rsx! {
+                    Markdown { src: "<X>\n<X>", components: components() }
+                },
+                // TODO:
+                // This gives nothing, when it should give both components with a space in between.
+                rsx! {
+                    p { style: "", class: "",
+                        span { style: "", class: "", "Content" }
+                        " "
+                        span { style: "", class: "", "Content" }
+                    }
+                },
+            )
+        });
+    }
 }
